10616ccbe1a2ca60f12beb70b19ffb48fa283262
John Heinnickel` <jheinnic@gmail.com>
2019-03-23 08:06:58 -0700

    Satisfy compilability of first GRPC service WIP

--- zos/src/apps/modules/features/gene-game/resolvers/ecosystem.resolver.ts
+++ zos/src/apps/modules/features/gene-game/resolvers/ecosystem.resolver.ts
@@ -2,6 +2,10 @@ import { Query } from '@nestjs/common';
 import { Resolver, Mutation, Args, ResolveProperty, Parent } from '@nestjs/graphql';
 import { EcosystemClient } from '../service/ecosystem-client.service';
 
+interface Temp {
+   id: number;
+}
+
 @Resolver('Ecosystem')
 export class EcosystemResolver {
    constructor(
@@ -9,17 +13,17 @@ export class EcosystemResolver {
    ) {}
 
    @Query('author')
-   async getAuthor(@Args('id') id: number) {
+   async getAuthor(@Args('id') _id: number) {
       // return await this.authorsService.findOneById(id);
    }
 
    @Mutation()
-   async upvotePost(@Args('postId') postId: number) {
+   async upvotePost(@Args('postId') _postId: number) {
       // return await this.postsService.upvoteById({ id: postId });
    }
 
    @ResolveProperty('posts')
-   async getPosts(@Parent() { id }) {
+   async getPosts(@Parent() { id }: Temp) {
       return await this.postsService.findAll({ authorId: id });
    }
 }
