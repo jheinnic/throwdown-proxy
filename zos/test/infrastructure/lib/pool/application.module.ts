a44c9b8462a73b4ed8e6050e87cf88d89ef40af6
John Heinnickel <jheinnic@hotmail.com>
2021-04-19 05:48:23 -0700

    ZOS Build

--- zos/test/infrastructure/lib/pool/application.module.ts
+++ zos/test/infrastructure/lib/pool/application.module.ts
@@ -1,20 +1,28 @@
 import {CoroutinesModule} from '@jchptf/coroutines';
-import {LoadResourcePoolStrategy, ResourceSemaphoreModule} from '@jchptf/semaphore';
+import {
+   SEMAPHORE_RESOURCE_POOL, SEMAPHORE_RESOURCE_POOL_PROVIDER_TOKEN, SemaphoreModule
+} from '@jchptf/semaphore';
 import {Canvas} from 'canvas';
+import { Module } from '@nestjs/common';
+import { DynamicProviderBindingStyle } from '@jchptf/nestjs';
+import { ApplicationModuleId } from './application.constants';
 
 @Module({
    imports: [
       CoroutinesModule,
-      ResourceSemaphoreModule.forRoot({
-            name: 'FourHundredSquare',
-            loadStrategy: LoadResourcePoolStrategy.EAGER_FIXED_ITERABLE,
-            resources: [
-               new Canvas(400, 400), new Canvas(400, 400),
-               new Canvas(400, 400), new Canvas(400, 400)
-            ],
+      SemaphoreModule.forFeature({
+            forModule: ApplicationModuleId,
+            [SEMAPHORE_RESOURCE_POOL]: {
+               style: DynamicProviderBindingStyle.VALUE,
+               provide: SEMAPHORE_RESOURCE_POOL_PROVIDER_TOKEN,
+               useValue: [
+                  new Canvas(400, 400), new Canvas(400, 400),
+                  new Canvas(400, 400), new Canvas(400, 400)
+               ],
+            }
          }
       )
    ]
 })
-export class ApplicationModule
+export class ApplicationModule extends ApplicationModuleId
 { }
