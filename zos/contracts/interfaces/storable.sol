12a014ad08423f40e13fb46dd8e4bc50b5ac4468
John Heinnickel <jheinnic@hotmail.com>
2021-04-19 05:49:38 -0700

    ZOS Contracts

--- zos/contracts/interfaces/storable.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.4.24;
-
-interface storable {
-    function getLedgerNameHash() public view returns (bytes32);
-    function getStorageNameHash() public view returns (bytes32);
-}
